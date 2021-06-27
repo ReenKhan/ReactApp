f413bca37cae82ea1e01662b24d8ee8f181c734f
ReenKhan <shireenk154@gmail.com>
2021-06-28 03:42:29 +0400

    first commit

--- src/components/TicketNumber.js
+++ src/components/TicketNumber.js
@@ -1,59 +1,62 @@
 import Number from './NumberSelected';
-import {useEffect, useState} from 'react';
+import {useState } from 'react';
 import '../css/TicketNumber.css'
-function TicketNumber(){
-     
-    const [numberSelected,setNumberSelected]= useState(JSON.stringify([]));
-    const [selectCount,setSelectCount]= useState(0);
-    const [cleared,setCleared]= useState(false);
-    const [total,setTotal]= useState([0]);
+function TicketNumber() {
 
+    const [numberSelected, setNumberSelected] = useState(JSON.stringify([]));
+    const [selectCount, setSelectCount] = useState(0);
+    const [cleared, setCleared] = useState(false);
+    const [total, setTotal] = useState([0]);
 
+    const numberArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-     const numberArr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-     const clear = () => {
+    const clear = () => {
         window.location.reload(false);
-        setTotal([])  
-        setNumberSelected([])  
-        setSelectCount(selectCount,1)
-        setCleared(true) 
+        setTotal([])
+        setNumberSelected([])
+        setSelectCount(selectCount, 1)
+        setCleared(true)
     }
 
-    const cashPressed = () =>{
+    const cashPressed = () => {
         const checkCount = parseInt(selectCount);
-        if(checkCount<5){
-           alert("Please select 5 numbers!")
+        if (checkCount < 5) {
+            alert("Please select 5 numbers!")
 
-            
         }
 
-        else{
-             alert("The total is:"+total )
+        else {
+            alert("The total is:" + total)
         }
     }
 
-    
-    const random=()=>{
-        alert("random buttons value from the array should be chosen")
+    const newArr = [];
+    function randomButton(Arr) {
+        var rand = Arr[Math.floor(Math.random() * Arr.length)];
+        if (!newArr.includes(rand)) {
+            newArr.push(rand);
+        } else {
+            randomButton(Arr);
+        }
     }
 
-    return(
+
+    return (
         <div className="TicketNumber">
             <div className="number-buttons">
-                {numberArr.map((num)=>{
-                    return <Number num={num}/>
+                {numberArr.map((num) => {
+                    return <Number num={num} />
                 })}
                 <button onClick={cashPressed} className="bigButtons">
                     Cash
                 </button>
                 <button onClick={clear} className="bigButtons">
-                    Clear    
-                </button>       
-                <button onClick={random} className="bigButtons">
-                    Random  
-                </button>      
-            </div> 
+                    Clear
+                </button>
+                <button onClick={randomButton(numberArr)} className="bigButtons">
+                    Random
+                </button>
+            </div>
         </div>
     )
 }
